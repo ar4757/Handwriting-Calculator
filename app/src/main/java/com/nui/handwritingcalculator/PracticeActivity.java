456807f1bdaf41a238494a648768aa9610ea9390
ar4757 <ar4757@gmail.com>
2020-02-10 22:28:37 -0500

    - Basic navbar setup that can be used on multiple screens
    - Basic practice mode layout (still needs work)

--- app/src/main/java/com/nui/handwritingcalculator/PracticeActivity.java
+++ app/src/main/java/com/nui/handwritingcalculator/PracticeActivity.java
@@ -1,20 +1,22 @@
 package com.nui.handwritingcalculator;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class PracticeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practice);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
