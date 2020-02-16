70b9afdb67aceb02b232d2e229819be270866b83
Mohit Garg <mohitgarg@ufl.edu>
2020-02-16 15:07:02 -0500

    Merge branch 'master' of https://github.com/ar4757/Handwriting-Calculator
    # Conflicts:
    #	app/src/main/java/com/nui/handwritingcalculator/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/nui/handwritingcalculator/PracticeSelect.java
+++ app/src/main/java/com/nui/handwritingcalculator/PracticeSelect.java
@@ -1,5 +1,6 @@
 package com.nui.handwritingcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,8 @@ public class PracticeSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.practice_select);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         //CheckBox variable added
         multiplyCheckbox = findViewById(R.id.multiply_checkbox);
@@ -72,7 +75,8 @@ public class PracticeSelect extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i = new Intent(PracticeSelect.this, PracticeActivity.class);
+                startActivity(i);
             }
         });
 
