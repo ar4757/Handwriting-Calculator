f6053847d9c3836cd485221edc1ec49aa4dfbe71
ar4757 <ar4757@gmail.com>
2020-02-17 14:47:52 -0500

    Improved gesture recognition

--- app/src/main/java/com/nui/handwritingcalculator/CalculatorActivity.java
+++ app/src/main/java/com/nui/handwritingcalculator/CalculatorActivity.java
@@ -2,26 +2,38 @@ package com.nui.handwritingcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.gesture.Prediction;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.TextViewCompat;
 
 import android.gesture.GestureLibraries;
 import android.gesture.GestureLibrary;
 import android.gesture.GestureOverlayView;
 import android.gesture.GestureOverlayView.OnGesturePerformedListener;
 import android.gesture.Gesture;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import static android.gesture.GestureStore.ORIENTATION_INVARIANT;
+import static android.gesture.GestureStore.SEQUENCE_INVARIANT;
+
 public class CalculatorActivity extends AppCompatActivity implements OnGesturePerformedListener {
 
     private GestureLibrary gLibrary;
+    private GestureOverlayView gOverlay;
+    private String mathExpressionString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +42,69 @@ public class CalculatorActivity extends AppCompatActivity implements OnGesturePe
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        gLibrary =
-                GestureLibraries.fromRawResource(this, R.raw.gesture);
+        gLibrary = GestureLibraries.fromRawResource(this, R.raw.gesture);
+        //gLibrary.setOrientationStyle(ORIENTATION_INVARIANT);
+        //gLibrary.setSequenceType(SEQUENCE_INVARIANT);
         if (!gLibrary.load()) {
             finish();
         }
 
-        GestureOverlayView gOverlay =
-                (GestureOverlayView) findViewById(R.id.handwriting);
+        gOverlay = (GestureOverlayView) findViewById(R.id.handwriting);
+        gOverlay.setGestureStrokeAngleThreshold(90.0f);
         gOverlay.addOnGesturePerformedListener(this);
+
+        mathExpressionString = "";
+
+        Button calculateButton = findViewById(R.id.calculate_btn);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView solutionText = findViewById(R.id.solution);
+                solutionText.setText("Solution to " + mathExpressionString + " goes here");
+            }
+        });
     }
 
     public void onGesturePerformed(GestureOverlayView overlay, Gesture
             gesture) {
-        ArrayList<Prediction> predictions =
-                gLibrary.recognize(gesture);
-
+        ArrayList<Prediction> predictions = gLibrary.recognize(gesture);
+        for (int i = 0; i < predictions.size(); i++) {
+            System.out.println("Prediction: " + predictions.get(i).name + " with score of " + predictions.get(i).score);
+        }
+        System.out.println();
         if (predictions.size() > 0 && predictions.get(0).score > 1.0) {
 
-            String action = predictions.get(0).name;
+            //Find the first (best scoring) gesture with the matching stroke count
+            String action = "invalid";
+            for (int i = 0; i < predictions.size(); i++) {
+                String temp_action = predictions.get(i).name;
+                if (gLibrary.getGestures(temp_action).get(0).getStrokesCount() == gesture.getStrokesCount()) {
+                    action = temp_action;
+                    break;
+                }
+            }
 
             Toast.makeText(this, action, Toast.LENGTH_SHORT).show();
+            if (!action.equals("invalid")) {
+                TextView recognizedText = new TextView(this);
+                recognizedText.setText(action);
+                recognizedText.setTextColor(Color.WHITE);
+                float x = gesture.getBoundingBox().left;
+                float y = gesture.getBoundingBox().top;
+                float height = gesture.getBoundingBox().height();
+                float width = gesture.getBoundingBox().width();
+                recognizedText.setX(x - 10);
+                recognizedText.setY(y - 90);
+                if (width > height) {
+                    recognizedText.setTextSize(width / 2);
+                } else {
+                    recognizedText.setTextSize(height / 2);
+                }
+                gOverlay.addView(recognizedText);
+                mathExpressionString += action;
+            }
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- app/src/main/java/com/nui/handwritingcalculator/HandwritingView.java
+++ app/src/main/java/com/nui/handwritingcalculator/HandwritingView.java
@@ -17,122 +17,17 @@ import java.util.ArrayList;
 
 public class HandwritingView extends GestureOverlayView {
 
-    public static final int DEFAULT_COLOR = Color.WHITE;
-    public static final int DEFAULT_BG_COLOR = Color.BLACK;
-    public static final int DEFAULT_WIDTH = 5;
-    //gesture variables
-    private Path stroke;
-    private ArrayList<Path> strokeList = new ArrayList<>();
-    private Paint strokePaint;
-    //canvas variables
-    private Bitmap bitmap;
-    private Canvas canvas;
-    private Paint bitmapPaint = new Paint(Paint.DITHER_FLAG);
-    
-    private float curX, curY; //current location on screen
 
     
     public HandwritingView(Context context) {
         this(context, null);
     }
 
-    public  HandwritingView(Context context, AttributeSet attrs) {
+    public HandwritingView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setup();
-    }
-
-    private void setup() {
-        //Set up stroke paint values
-        
-        strokePaint = new Paint();
-        strokePaint.setColor(DEFAULT_COLOR);
-        strokePaint.setAntiAlias(true);
-        strokePaint.setDither(true);
-        strokePaint.setXfermode(null);
-        strokePaint.setMaskFilter(null);
-        strokePaint.setAlpha(0xff);
-        strokePaint.setStrokeWidth(DEFAULT_WIDTH);
-        strokePaint.setStyle(Paint.Style.STROKE);
-        strokePaint.setStrokeJoin(Paint.Join.ROUND);
-        strokePaint.setStrokeCap(Paint.Cap.ROUND);
 
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h , int oldw, int oldh) {
-        //Redraw the canvas
-        
-        super.onSizeChanged(w,h,oldw, oldh);
-
-        bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        canvas = new Canvas(bitmap);
-
-    }
-
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-       // canvas.save();  //This doesn't seem to be necessary
-        
-        canvas.drawColor(DEFAULT_BG_COLOR);
-
-        //draw the strokes on the canvas
-        for (Path fp : strokeList) {
-            canvas.drawPath(fp, strokePaint);
-        }
-
-        canvas.drawBitmap(bitmap, 0, 0, bitmapPaint);
-       //canvas.restore();  //This doesn't seem to be necessary
-    }
-
-    private void strokeBegin(float x, float y) {
-        stroke = new Path();
-        strokeList.add(stroke);
-
-        stroke.reset();
-        stroke.moveTo(x, y);
-        curX = x;
-        curY = y;
-    }
-
-    private void strokeMove(float x, float y) {
-
-        stroke.quadTo(curX, curY, (x + curX) / 2, (y + curY) / 2);
-        curX = x;
-        curY = y;
-    }
-
-    private void strokeEnd() {
-        stroke.lineTo(curX, curY);
-    }
-
-    public void clearScreen() {
-        //Clear the display in handwriting area
-        //Should we also clear the formula area?
-
-        strokeList.clear();
-        invalidate();
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
-
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN :
-                strokeBegin(x, y);
-                break;
-            case MotionEvent.ACTION_MOVE :
-                strokeMove(x, y);
-                break;
-            case MotionEvent.ACTION_UP :
-                strokeEnd();
-                break;
-        }
-        invalidate();
-        return true;
-    }
 
 
 }
