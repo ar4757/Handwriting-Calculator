fbce947d62cf1c31559164039d5bf6e8c6a55979
dospina3 <daniel_ospina@live.com>
2020-03-03 17:18:42 -0500

    Help scrollView update

--- app/src/main/java/com/nui/handwritingcalculator/help.java
+++ app/src/main/java/com/nui/handwritingcalculator/help.java
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class help extends AppCompatActivity {
 
+    private ScrollView scrollView;
+    private TextView calc_help;
+    private TextView prac_help;
+
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
@@ -31,8 +41,42 @@ public class help extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
 
 
+        final Button calcHelpButton = findViewById(R.id.calc_help_button);
+        final Button pracHelpButton = findViewById(R.id.practice_help_button);
+
+        scrollView = findViewById(R.id.scrollView_help);
+        calc_help = findViewById(R.id.Calculator_Help);
+        prac_help = findViewById(R.id.Practice_Help);
 
 
+        calcHelpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                focusOnView(calc_help);
+                pracHelpButton.setPaintFlags(pracHelpButton.getPaintFlags() & (~ Paint.UNDERLINE_TEXT_FLAG));
+                calcHelpButton.setPaintFlags(calcHelpButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            }
+        });
+
+        pracHelpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                focusOnView(prac_help);
+                calcHelpButton.setPaintFlags(calcHelpButton.getPaintFlags() & (~ Paint.UNDERLINE_TEXT_FLAG));
+                pracHelpButton.setPaintFlags(pracHelpButton.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+            }
+        });
 
     }
+
+
+    private void focusOnView(final TextView t){
+        new Handler().post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.scrollTo(0, t.getTop());
+            }
+        });
+    }
+
 }
